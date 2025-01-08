b1d1d1fbe283d5072a63d3789b432fcd85f0778e
thenoob213 <159505087+thenoob213@users.noreply.github.com>
2025-01-09 04:10:19 +0700

    add pause menu

---
+++ Scenes/Levels/pause_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"pause"):
+		if visible:
+			visible = false
+		else :
+			visible = true
+
+func _on_resume_pressed() -> void:
+	visible = false
+
+func _on_restart_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/world.tscn")
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu/main_menu.tscn")
