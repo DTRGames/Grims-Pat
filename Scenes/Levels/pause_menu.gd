a3523b6e958bc9979d93d2d989d24ffa24d71283
DTRGames <dilendilen407@gmail.com>
2025-01-09 17:36:49 +0200

    added a settings menu

--- Scenes/Levels/pause_menu.gd
+++ Scenes/Levels/pause_menu.gd
@@ -2,12 +2,25 @@ extends Control
 
 var stop : bool = false
 
+@onready var panel = $Panel
+@onready var window_button = $Settings/WindowButton
+@onready var settings_back = $Settings/SettingsBack
+@onready var label = $Settings/Label
+@onready var settings = $Settings
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"escape") and stop == false:
 		if visible:
 			visible = false
 		else :
 			visible = true
+
+func update_display():
+	
+	window_button.text = "windowed"
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		window_button.text = "FullScreen"
 
 func _on_resume_pressed() -> void:
 	visible = false
@@ -17,3 +30,31 @@ func _on_restart_pressed() -> void:
 
 func _on_main_menu_pressed() -> void:
 	SceneTransion.transion("res://Scenes/MainMenu/main_menu.tscn")
+
+
+func _on_window_button_pressed():
+	var mode := DisplayServer.window_get_mode()
+	if mode != DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+	
+	update_display()
+
+
+func _on_settings_back_pressed():
+	panel.visible = true
+	window_button.visible = false
+	label.visible = false
+	settings_back.visible = false
+	settings.visible = false
+
+
+func _on_settings_pressed():
+	panel.visible = false
+	window_button.visible = true
+	label.visible = true
+	settings_back.visible = true
+	settings.visible = true
--- Scenes/MainMenu/main_menu.gd
+++ Scenes/MainMenu/main_menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@onready var window_button = $Settings/WindowButton
+@onready var settings = $Settings
+@onready var settings_button = $SettingsButton
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"escape"):
 		if $Difficulty.visible:
@@ -7,10 +11,15 @@ func _input(event: InputEvent) -> void:
 			$Title.visible = true
 			$Start.visible = true
 			$Quit.visible = true
+			settings_button.visible = true
 			$Difficulty.visible = false
+
+func _ready():
+	update_display()
 
 func _on_start_pressed() -> void:
 	$Difficulty.visible = true
+	settings_button.visible = false
 	$Title.visible = false
 	$Start.visible = false
 	$Quit.visible = false
@@ -45,4 +54,42 @@ func _on_back_pressed() -> void:
 	$Title.visible = true
 	$Start.visible = true
 	$Quit.visible = true
+	settings_button.visible = true
 	$Difficulty.visible = false
+
+
+func _on_settings_pressed():
+	$Difficulty/Description.visible = false
+	$Title.visible = false
+	$Start.visible = false
+	$Quit.visible = false
+	$Difficulty.visible = false
+	settings.visible = true
+	settings_button.visible = false
+
+func update_display():
+	
+	window_button.text = "windowed"
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		window_button.text = "FullScreen"
+
+func _on_button_pressed():
+	var mode := DisplayServer.window_get_mode()
+	if mode != DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+	
+	update_display()
+
+
+func _on_settings_back_pressed():
+	$Difficulty/Description.visible = false
+	$Title.visible = true
+	$Start.visible = true
+	$Quit.visible = true
+	$Difficulty.visible = false
+	settings_button.visible = true
+	settings.visible = false
